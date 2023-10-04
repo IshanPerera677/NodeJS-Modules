1ea7e231c354747027ed6a86d3014ae877d926d6
IshanPerera677 <146979990+IshanPerera677@users.noreply.github.com>
2023-10-04 22:38:34 +0530

    Update no-cluster.js

--- NodeJS-Cluster-Module/no-cluster.js
+++ NodeJS-Cluster-Module/no-cluster.js
@@ -1,10 +1,1 @@
-const http = require("node:http");
 
-const server = http.createServer((req, res) =>{
-    if(req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Home page");
-    } 
-});
-
-server.listen(8000, () => console.log("Server is running on port 8000"));
