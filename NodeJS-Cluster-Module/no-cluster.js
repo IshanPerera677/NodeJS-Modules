18a1cf7a49f52e23c89223adbdc8490adae2bc65
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2023-10-05 06:58:36 +0530

    Update no-cluster.js
    Added slow-page endpoint

--- NodeJS-Cluster-Module/no-cluster.js
+++ NodeJS-Cluster-Module/no-cluster.js
@@ -4,7 +4,11 @@ const server = http.createServer((req, res) =>{
     if(req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Home page");
-    } 
+    } else if(req.url === "/slow-page"){
+        for(let i = 0; i < 60000000; i++){} //Simulate CPU work
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Slow page");
+    }
 });
 
 server.listen(8000, () => console.log("Server is running on port 8000"));
