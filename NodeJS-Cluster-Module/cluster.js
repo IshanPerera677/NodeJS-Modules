313050b19c3f669058b68525fc0aedaf249995f0
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2023-10-05 07:35:43 +0530

    Update cluster.js

--- NodeJS-Cluster-Module/cluster.js
+++ NodeJS-Cluster-Module/cluster.js
@@ -1,3 +1,9 @@
 //=: Cluster Module
 const cluster = require("node:cluster");
 const http = require("node:http");
+
+if(cluster.isMaster) {
+    console.log(`Master process ${process.pid} is running`);
+    cluster.fork();
+    cluster.fork();
+}
